ec9f3960b266400e473e20a2b8c31ebfa1255599
Guilherme2080 <98264738+Guilherme2080@users.noreply.github.com>
2022-05-15 08:53:37 -0300

    Add files via upload

---
+++ site/index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="estilo.css" media="screen" />
+    <title>Site Plupio</title>
+</head>
+
+<body>
+
+    <header>
+        <div class="status">
+
+            <div class="h_horario">
+                <p>9:41</p>
+            </div>
+
+            <div class="h_bateria">
+                <img src="imagens/bateria.jpg">
+            </div>
+        </div>
+
+        <div class="loc_title">
+            <p>Plupio</p>
+            <article>São Paulo - SP <img src="imagens/localizacao1.png"></article>
+        </div>
+    </header>
+
+    <div class="c_opcoes">
+
+        <div class="circulos">
+            <img src="imagens/1.jpg">
+            <p>Consultas</p>
+        </div>
+
+        <div class="circulos">
+            <img src="imagens/2.jpg">
+            <p>Exames</p>
+        </div>
+
+        <div class="circulos">
+            <img src="imagens/4.jpg">
+            <p>Medicamentos</p>
+
+        </div>
+    </div>
+
+    <div class="painel">
+        <div class="p_imagem">
+            <img src="imagens/painel1.jpg">
+        </div>
+        <div class="p_texto">
+            <p>Telemedicina com mais de 20 especialidades na palma de sua mão</p>
+
+        </div>
+    </div>
+
+    <div class="beneficios">
+        <h4>Beneficios</h4>
+        <div class="b_qd">
+            <div class="b_circle">
+                <p>Assistencia residencial</p>
+            </div>
+
+            <div class="b_circle">
+            <p>cupons</p>
+            </div>
+        </div>
+
+        <div class="b_qd">
+            <div class="b_circle">
+            <p>Seguro de Vida</p>
+            </div>
+
+            <div class="b_circle">
+            <p>Restaurantes</p>
+            </div>
+        </div>
+
+        <div class="b_qd">
+            <div class="b_circle">
+            <p>Cartao internacional</p>
+            </div>
+
+            <div class="b_circle">
+            <p>Conta digital</p>
+
+            </div>
+        </div>
+
+        <div class="b_qd">
+            <div class="b_circle">
+            <p>Sorteio de premios</p>
+            </div>
+
+            <div class="b_circle">
+            <p>Auxilio funeral</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="requisitados">
+        <h4>Os mais requisitados</h4>
+    </div>
+
+    <div class="agendamentos">
+        <h4>Ultimos agendamentos</h4>
+
+    </div>
+
+    <div class="consultas">
+        <h4>Consultas e exames realizados</h4>
+
+    </div>
+
+
+    <footer>
+
+    </footer>
+
+</body>
+
+</html>
